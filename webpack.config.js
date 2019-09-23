094bdd49aed3631476677e3dad1d58e16173a426
Venkatraman <ramsunvtech@gmail.com>
2019-09-24 04:04:31 +0800

    Basic SetUp App

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['*', '.js'],
+    },
+
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'app-bundle.js'
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+        })
+    ],
+
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    }
+}
